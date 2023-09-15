fa7a02622f5dce80f496c56494229fb6947d07f0
haoyiwen <yw.hao@trip.com>
2023-09-15 21:24:51 +0800

    feat: 添加minio文件系统

--- README.md
+++ README.md
@@ -5,7 +5,14 @@
 ### 课程基础
 ### 课程计划
 ### 师资管理
+### 媒资管理
 
 # 技术栈
 
-spring cloud + spring boot + redis + nacos + mybatis plus + mysql
+spring cloud + spring boot + redis + nacos + mybatis plus + mysql + minio
+
+## 说明
+
+因为nacos对服务器要求较高，且arm架构对虚拟机支持不太友好，所以该系统所有部署在arm架构macbook完成。
+
+minio部署在mac本地。
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
@@ -0,0 +1,68 @@
+package com.xuecheng.media;
+
+import com.alibaba.nacos.common.utils.IoUtils;
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import io.minio.*;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.io.*;
+
+public class MinioTest {
+
+    static MinioClient minioClient =
+            MinioClient.builder()
+                    .endpoint("http://127.0.0.1:9000")
+                    .credentials("minio", "hyw650022")
+                    .build();
+
+    @Test
+    public void test_upload() throws Exception {
+//        通过扩展名称得到媒体资源类型 mimeType
+        ContentInfo extensionMath = ContentInfoUtil.findExtensionMatch(".jpeg");
+        String mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+        if(extensionMath !=null) {
+            mimeType = extensionMath.getMimeType();
+        }
+
+//        上传文件信息
+        UploadObjectArgs yiwen = UploadObjectArgs.builder()
+                .bucket("yiwen")
+                .filename("/Users/yw.hao/Documents/WechatIMG1.jpeg")
+                .object("test/01/test.jpeg")
+                .contentType(mimeType)
+                .build();
+
+//        上传文件参数信息
+        minioClient.uploadObject(yiwen);
+    }
+
+    @Test
+    public void test_delete() throws Exception {
+        RemoveObjectArgs yiwen = RemoveObjectArgs.builder().bucket("yiwen").object("test.jpeg").build();
+        //        删除文件
+        minioClient.removeObject(yiwen);
+    }
+
+    @Test
+    public void test_getFile() throws Exception{
+        GetObjectArgs yiwen = GetObjectArgs.builder().bucket("yiwen").object("test/01/test.jpeg").build();
+
+        FilterInputStream inputStream = minioClient.getObject(yiwen);
+
+        FileOutputStream outputStream = new FileOutputStream(new File("/Users/yw.hao/Downloads/test.jpeg"));
+
+        IoUtils.copy(inputStream, outputStream);
+
+//        校验文件的完整性
+        String source_md5 = DigestUtils.md5Hex(new FileInputStream(new File("/Users/yw.hao/Documents/WechatIMG1.jpeg")));
+//        本地文件md5
+        String local_md5 = DigestUtils.md5Hex(new FileInputStream(new File("/Users/yw.hao/Downloads/test.jpeg")));
+        if(source_md5.equals(local_md5)){
+            System.out.println("下载成功");
+        }
+
+    }
+}
