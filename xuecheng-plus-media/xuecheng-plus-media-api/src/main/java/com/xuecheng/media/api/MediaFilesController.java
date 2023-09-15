d1f8a5746043d2bc9ed73406d2c11827cc0eaae5
haoyiwen <yw.hao@trip.com>
2023-09-15 22:45:55 +0800

    feat: 删除logs

--- xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/MediaFilesController.java
+++ xuecheng-plus-media/xuecheng-plus-media-api/src/main/java/com/xuecheng/media/api/MediaFilesController.java
@@ -1,38 +1,43 @@
 package com.xuecheng.media.api;
 
+import com.alibaba.nacos.common.http.param.MediaType;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
 import com.xuecheng.media.service.MediaFileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
- * @description 媒资文件管理接口
  * @author Mr.M
- * @date 2022/9/6 11:29
  * @version 1.0
+ * @description 媒资文件管理接口
+ * @date 2022/9/6 11:29
  */
- @Api(value = "媒资文件管理接口",tags = "媒资文件管理接口")
- @RestController
+@Api(value = "媒资文件管理接口", tags = "媒资文件管理接口")
+@RestController
 public class MediaFilesController {
 
 
-  @Autowired
-  MediaFileService mediaFileService;
+    @Autowired
+    MediaFileService mediaFileService;
 
 
- @ApiOperation("媒资列表查询接口")
- @PostMapping("/files")
- public PageResult<MediaFiles> list(PageParams pageParams, @RequestBody QueryMediaParamsDto queryMediaParamsDto){
-  Long companyId = 1232141425L;
-  return mediaFileService.queryMediaFiels(companyId,pageParams,queryMediaParamsDto);
+    @ApiOperation("媒资列表查询接口")
+    @PostMapping("/files")
+    public PageResult<MediaFiles> list(PageParams pageParams, @RequestBody QueryMediaParamsDto queryMediaParamsDto) {
+        Long companyId = 1232141425L;
+        return mediaFileService.queryMediaFiels(companyId, pageParams, queryMediaParamsDto);
+    }
 
- }
-
+    @ApiOperation("上传图片")
+    @RequestMapping(value = "/upload/coursefile", consumes = MediaType.MULTIPART_FORM_DATA)
+    public UploadFileResultDto upload(@RequestPart("filedata")MultipartFile filedata) {
+        return null;
+    }
 }
---
+++ xuecheng-plus-media/xuecheng-plus-media-model/src/main/java/com/xuecheng/media/model/dto/UploadFileResultDto.java
@@ -0,0 +1,11 @@
+package com.xuecheng.media.model.dto;
+
+import com.xuecheng.media.model.po.MediaFiles;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class UploadFileResultDto extends MediaFiles {
+
+}
