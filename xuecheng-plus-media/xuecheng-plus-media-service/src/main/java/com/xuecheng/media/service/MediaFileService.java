6fed7eb0ce39f8148b9ea2d9c27b882f0beb5837
haoyiwen <yw.hao@trip.com>
2023-09-17 18:00:38 +0800

    feat: 完成分块上传

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -72,4 +72,17 @@ public interface MediaFileService {
      * @date 2022/9/13 15:50
      */
     public RestResponse uploadChunk(String fileMd5,int chunk,String localChunkFilePath);
+
+
+    /**
+     * @description 合并分块
+     * @param companyId  机构id
+     * @param fileMd5  文件md5
+     * @param chunkTotal 分块总和
+     * @param uploadFileParamsDto 文件信息
+     * @return com.xuecheng.base.model.RestResponse
+     * @author Mr.M
+     * @date 2022/9/13 15:56
+     */
+    public RestResponse mergechunks(Long companyId,String fileMd5,int chunkTotal,UploadFileParamsDto uploadFileParamsDto);
 }
