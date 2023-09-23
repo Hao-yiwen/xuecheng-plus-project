119218df66bbdd3c780fb6fb2ea9425954936897
haoyiwen <yw.hao@trip.com>
2023-09-24 00:30:39 +0800

    feat: 完成课程发布模块

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/feignclient/SearchServiceClientFallbackFactory.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.feignclient;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SearchServiceClientFallbackFactory implements FallbackFactory<SearchServiceClient> {
+    @Override
+    public SearchServiceClient create(Throwable throwable) {
+        return new SearchServiceClient() {
+            @Override
+            public Boolean add(CourseIndex courseIndex) {
+                log.error("添加课程所以发生熔断，索引信息{}",courseIndex);
+                // 走降级返回false
+                return false;
+            }
+        };
+    }
+}
